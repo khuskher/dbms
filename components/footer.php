d1d1f77a673e18bf394e6cdef18de476bd999170
khuskher <91379784+khuskher@users.noreply.github.com>
2023-04-29 01:03:31 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+
+   <div class="credit">U21CS003|U21CS008|U21CS056|U21CS067</div>
+
+</footer>
+
+<!-- <div class="loader">
+   <img src="images/loader.gif" alt="">
+</div> -->
